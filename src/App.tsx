8476e7cc82e77386bfe38dd1fa2cc3e56a4d177f
irmaLaude <laudelyrma@gmail.com>
2024-07-28 18:40:54 -0300

    Ejercicio1

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React LUCAaaa</h1>
+      <h1>INFORMATORIO 2024!!!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
